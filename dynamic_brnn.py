9c2f363943cd9b39807315fa4fa452e7c4eb82c9
Xu Ziyang <xuziyang@pku.edu.cn>
2018-04-20 00:45:31 +0800

    clean up

--- dynamic_brnn.py
+++ dynamic_brnn.py
@@ -11,8 +11,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops.rnn import bidirectional_dynamic_rnn
 
-from speechvalley.utils import describe, dropout
-from speechvalley.utils import lnBasicRNNCell, lnGRUCell, lnBasicLSTMCell
+# from speechvalley.utils import describe, dropout
+from utils import describe
 
 def build_multi_dynamic_brnn(args,
                              maxTimeSteps,
@@ -42,7 +42,7 @@ def build_multi_dynamic_brnn(args,
         shape = output_fb.get_shape().as_list()
         output_fb = tf.reshape(output_fb, [shape[0], shape[1], 2, int(shape[2] / 2)])
         hidden = tf.reduce_sum(output_fb, 2)
-        hidden = dropout(hidden, args.keep_prob, (args.mode == 'train'))
+        # hidden = dropout(hidden, args.keep_prob, (args.mode == 'train')) #Apr 19
 
         if i != args.num_layer - 1:
             hid_input = hidden
@@ -58,25 +58,7 @@ class DBiRNN(object):
     def __init__(self, args, maxTimeSteps):
         self.args = args
         self.maxTimeSteps = maxTimeSteps
-        if args.layerNormalization is True:
-            if args.rnncell == 'rnn':
-                self.cell_fn = lnBasicRNNCell
-            elif args.rnncell == 'gru':
-                self.cell_fn = lnGRUCell
-            elif args.rnncell == 'lstm':
-                self.cell_fn = lnBasicLSTMCell
-            else:
-                raise Exception("rnncell type not supported: {}".format(args.rnncell))
-        else:
-            if args.rnncell == 'rnn':
-                self.cell_fn = tf.contrib.rnn.BasicRNNCell
-            elif args.rnncell == 'gru':
-                self.cell_fn = tf.contrib.rnn.GRUCell
-            elif args.rnncell == 'lstm':
-                self.cell_fn = tf.contrib.rnn.BasicLSTMCell
-            else:
-                raise Exception("rnncell type not supported: {}".format(args.rnncell))
-
+        self.cell_fn = tf.contrib.rnn.BasicLSTMCell
         self.build_graph(args, maxTimeSteps)
 
     @describe
@@ -86,12 +68,7 @@ class DBiRNN(object):
             self.inputX = tf.placeholder(tf.float32,
                                          shape=(maxTimeSteps, args.batch_size, args.num_feature))  # [maxL,32,39]
             inputXrs = tf.reshape(self.inputX, [-1, args.num_feature])
-            # self.inputList = tf.split(0, maxTimeSteps, inputXrs) #convert inputXrs from [32*maxL,39] to [32,maxL,39]
             self.inputList = tf.split(inputXrs, maxTimeSteps, 0)  # convert inputXrs from [32*maxL,39] to [32,maxL,39]
-            self.targetIxs = tf.placeholder(tf.int64)
-            self.targetVals = tf.placeholder(tf.int32)
-            self.targetShape = tf.placeholder(tf.int64)
-            self.targetY = tf.SparseTensor(self.targetIxs, self.targetVals, self.targetShape)
             self.seqLengths = tf.placeholder(tf.int32, shape=(args.batch_size))
             self.config = {'name': args.model,
                            'rnncell': self.cell_fn,
@@ -99,9 +76,6 @@ class DBiRNN(object):
                            'num_hidden': args.num_hidden,
                            'num_class': args.num_class,
                            'activation': args.activation,
-                           'optimizer': args.optimizer,
-                           'learning rate': args.learning_rate,
-                           'keep prob': args.keep_prob,
                            'batch size': args.batch_size}
 
             fbHrs = build_multi_dynamic_brnn(self.args, maxTimeSteps, self.inputX, self.cell_fn, self.seqLengths)
@@ -112,21 +86,11 @@ class DBiRNN(object):
                     biasesClasses = tf.Variable(tf.zeros([args.num_class]), name='biasesClasses')
                     logits = [tf.matmul(t, weightsClasses) + biasesClasses for t in fbHrs]
             logits3d = tf.stack(logits)
-            self.loss = tf.reduce_mean(tf.nn.ctc_loss(self.targetY, logits3d, self.seqLengths))
-            self.var_op = tf.global_variables()
-            self.var_trainable_op = tf.trainable_variables()
+            # self.loss = tf.reduce_mean(tf.nn.ctc_loss(self.targetY, logits3d, self.seqLengths)) #Apr 19
 
-            if args.grad_clip == -1:
-                # not apply gradient clipping
-                self.optimizer = tf.train.AdamOptimizer(args.learning_rate).minimize(self.loss)
-            else:
-                # apply gradient clipping
-                grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, self.var_trainable_op), args.grad_clip)
-                opti = tf.train.AdamOptimizer(args.learning_rate)
-                self.optimizer = opti.apply_gradients(zip(grads, self.var_trainable_op))
             self.predictions = tf.to_int32(
                 tf.nn.ctc_beam_search_decoder(logits3d, self.seqLengths, merge_repeated=False)[0][0])
-            if args.level == 'cha':
-                self.errorRate = tf.reduce_sum(tf.edit_distance(self.predictions, self.targetY, normalize=True))
+            # if args.level == 'cha':
+            #     self.errorRate = tf.reduce_sum(tf.edit_distance(self.predictions, self.targetY, normalize=True))
             self.initial_op = tf.global_variables_initializer()
             self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=5, keep_checkpoint_every_n_hours=1)
--- libri_inference.py
+++ libri_inference.py
@@ -3,104 +3,69 @@ from sklearn import preprocessing
 from calcmfcc import calcfeat_delta_delta
 
 import time
-import datetime
 import os
-from six.moves import cPickle
-from functools import wraps
 import random
 
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import ctc_ops as ctc
 
-from speechvalley.utils import load_batched_data, describe, output_to_sequence, list_dirs, logging, count_params, get_num_classes, check_path_exists, dotdict, activation_functions_dict, optimizer_functions_dict
-from speechvalley.models import DBiRNN
+# from speechvalley.utils import count_params
+from dynamic_brnn import DBiRNN
+from utils import dotdict, describe, output_to_sequence
 
-from tensorflow.python.platform import flags
-from tensorflow.python.platform import app
+args['mode'] = 'test'
+args['level'] = 'cha'
+args['model'] = 'DBiRNN'
+args['rnncell'] = 'lstm'
+args['num_layer'] = 2
+args['activation'] = 'tanh'
+args['batch_size'] = 1
+args['num_hidden'] = 256
+args['num_feature'] = 39
+args['num_classes'] = 30
+args['num_epochs'] = 1
+args['datadir'] = '/home/zyxu/test'
+args['savedir'] = '/home/zyxu/test'
+args = dotdict(args)
 
-mode = 'mfcc'
-feature_len =13
-win_step = 0.01
-win_len = 0.02
+def getFeature(filename, mode = 'mfcc', feature_len =13, win_step = 0.01, win_len = 0.02)
+	(rate,sig)= wav.read(filename)
+	feat = calcfeat_delta_delta(sig,rate,
+	    win_length=win_len,win_step=win_step,mode=mode,feature_len=feature_len)
+	feat = preprocessing.scale(feat)
+	feat = np.transpose(feat)
+	return feat
 
-(rate,sig)= wav.read(fullFilename)
-feat = calcfeat_delta_delta(sig,rate,
-    win_length=win_len,win_step=win_step,mode=mode,feature_len=feature_len)
-feat = preprocessing.scale(feat)
-feat = np.transpose(feat)
+def getResult(audio):
 
+    feat = getFeature(audio_file)
+    seqLength = feat.shape[1]
+    maxTimeSteps = feat.shape[1]
+    model = DBiRNN(args, maxTimeSteps)
 
-def run(self):
-    # load data
-    args_dict = self._default_configs()
-    args = dotdict(args_dict)
-    feature_dirs, label_dirs = get_data(datadir, test_dataset)
-    batchedData, maxTimeSteps, totalN = self.load_data(feature_dirs[0], label_dirs[0], mode, level)
-    model = model_fn(args, maxTimeSteps)
+    # num_params = count_params(model, mode='trainable')
+    # all_num_params = count_params(model, mode='all')
+    # model.config['trainable params'] = num_params
+    # model.config['all params'] = all_num_params
+    print(model.config)
 
-    ## shuffle feature_dir and label_dir by same order
-    FL_pair = list(zip(feature_dirs, label_dirs))
-    random.shuffle(FL_pair)
-    feature_dirs, label_dirs = zip(*FL_pair)
+    with tf.Session(graph=model.graph) as sess:
+        # restore from stored model
+        savedir = args['savedir']
+        ckpt = tf.train.get_checkpoint_state(savedir)
+        if ckpt and ckpt.model_checkpoint_path:
+            model.saver.restore(sess, ckpt.model_checkpoint_path)
+            print('Model restored from:' + savedir)
 
-    for feature_dir, label_dir in zip(feature_dirs, label_dirs):
-        id_dir = feature_dirs.index(feature_dir)
-        print('dir id:{}'.format(id_dir))
-        batchedData, maxTimeSteps, totalN = self.load_data(feature_dir, label_dir, mode, level)
-        model = model_fn(args, maxTimeSteps)
-        num_params = count_params(model, mode='trainable')
-        all_num_params = count_params(model, mode='all')
-        model.config['trainable params'] = num_params
-        model.config['all params'] = all_num_params
-        print(model.config)
-        with tf.Session(graph=model.graph) as sess:
-            # restore from stored model
-            if keep == True:
-                ckpt = tf.train.get_checkpoint_state(savedir)
-                if ckpt and ckpt.model_checkpoint_path:
-                    model.saver.restore(sess, ckpt.model_checkpoint_path)
-                    print('Model restored from:' + savedir)
-            else:
-                print('Initializing')
-                sess.run(model.initial_op)
+	    batchInputs = [feat]
+	    batchSeqLengths = [seqLength]
+	    feedDict = {model.inputX: batchInputs, model.seqLengths: batchSeqLengths}
 
-            for epoch in range(num_epochs):
-                ## training
-                start = time.time()
-                if mode == 'train':
-                    print('Epoch {} ...'.format(epoch + 1))
+	    l, pre = sess.run([model.loss, model.predictions], feed_dict=feedDict)
 
-                batchErrors = np.zeros(len(batchedData))
-                batchRandIxs = np.random.permutation(len(batchedData))
+	    batchErrors[batch] = er
+	    print('\n{} mode, total:{},subdir:{}/{},batch:{}/{},test loss={:.3f},mean test CER={:.3f}\n'.format(
+	        level, totalN, id_dir+1, len(feature_dirs), batch+1, len(batchRandIxs), l, er/batch_size))
 
-                for batch, batchOrigI in enumerate(batchRandIxs):
-                    batchInputs, batchTargetSparse, batchSeqLengths = batchedData[batchOrigI]
-                    batchTargetIxs, batchTargetVals, batchTargetShape = batchTargetSparse
-                    feedDict = {model.inputX: batchInputs, model.targetIxs: batchTargetIxs,
-                                model.targetVals: batchTargetVals, model.targetShape: batchTargetShape,
-                                model.seqLengths: batchSeqLengths}
-
-                    if level == 'cha':
-                        l, pre, y, er = sess.run([model.loss, model.predictions,
-                            model.targetY, model.errorRate], feed_dict=feedDict)
-                        batchErrors[batch] = er
-                        print('\n{} mode, total:{},subdir:{}/{},batch:{}/{},test loss={:.3f},mean test CER={:.3f}\n'.format(
-                            level, totalN, id_dir+1, len(feature_dirs), batch+1, len(batchRandIxs), l, er/batch_size))
-
-                    # NOTE:
-                    if er / batch_size == 1.0:
-                        break
-
-                    if batch % 20 == 0:
-                        print('Truth:\n' + output_to_sequence(y, type=level))
-                        print('Output:\n' + output_to_sequence(pre, type=level))
-
-                if mode=='test' or mode=='dev':
-                    with open(os.path.join(resultdir, level + '_result.txt'), 'a') as result:
-                        result.write(output_to_sequence(y, type=level) + '\n')
-                        result.write(output_to_sequence(pre, type=level) + '\n')
-                        result.write('\n')
-                    epochER = batchErrors.sum() / totalN
-                    print(' test error rate:', epochER)
-                    logging(model, logfile, epochER, mode=mode)
+	    print('Output:\n' + output_to_sequence(pre, type=level))
