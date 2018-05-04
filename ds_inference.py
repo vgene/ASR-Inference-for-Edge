48ef29c6a16344f382bed3caca272b62d5204000
Xu Ziyang <xuziyang@pku.edu.cn>
2018-05-04 20:43:02 +0800

    add edge server

--- ds_inference.py
+++ ds_inference.py
@@ -2,16 +2,15 @@
 from __future__ import absolute_import, division, print_function
 from timeit import default_timer as timer
 
-import argparse
-import subprocess
 import sys
-import scipy.io.wavfile as wav
 import numpy as np
-from deepspeech.model import Model
-from utils import dotdict
+import python_bind
+import pickle
 
 def preprocess_audio(audio_path):
 
+    import subprocess
+    import scipy.io.wavfile as wav
     fs, audio = wav.read(audio_path)
     if fs != 16000:
         if fs < 16000:
@@ -33,8 +32,66 @@ def preprocess_audio(audio_path):
     audio = np.fromstring(output, dtype=np.int16)
     return 16000, audio
 
+def ds_server():
+    args = get_args()
+
+    from deepspeech.model import Model
+    # Beam width used in the CTC decoder when building candidate transcriptions
+    BEAM_WIDTH = 500
+
+    # The alpha hyperparameter of the CTC decoder. Language Model weight
+    LM_WEIGHT = 1.75
+
+    # The beta hyperparameter of the CTC decoder. Word insertion weight (penalty)
+    WORD_COUNT_WEIGHT = 1.00
+
+    # Valid word insertion weight. This is used to lessen the word insertion penalty
+    # when the inserted word is part of the vocabulary
+    VALID_WORD_COUNT_WEIGHT = 1.00
+
+    # Number of MFCC features to use
+    N_FEATURES = 26
+
+    # Size of the context window used for producing timesteps in the input vector
+    N_CONTEXT = 9
+
+    print('Loading model from file %s' % (args.model), file=sys.stderr)
+    model_load_start = timer()
+    ds = Model(args.model, N_FEATURES, N_CONTEXT, args.alphabet, BEAM_WIDTH)
+    model_load_end = timer() - model_load_start
+    print('Loaded model in %0.3fs.' % (model_load_end), file=sys.stderr)
+
+    if args.lm and args.trie:
+        print('Loading language model from files %s %s' % (args.lm, args.trie), file=sys.stderr)
+        lm_load_start = timer()
+        ds.enableDecoderWithLM(args.alphabet, args.lm, args.trie, LM_WEIGHT,
+                               WORD_COUNT_WEIGHT, VALID_WORD_COUNT_WEIGHT)
+        lm_load_end = timer() - lm_load_start
+        print('Loaded language model in %0.3fs.' % (lm_load_end), file=sys.stderr)
+
+
+    print('Start Deep Speech Server.')
+    python_bind.init_cloud_server()
+
+    while True:
+        try:
+            (fs, audio) = pickle.loads(python_bind.recv())
+            inference_start = timer()
+            result = ds.stt(audio, fs)
+            print(result)
+            inference_end = timer() - inference_start
+            audio_length = len(audio)*(1/fs)
+            print('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_length), file=sys.stderr)
+            pld = pickle.dumps(result, 2)
+            python_bind.send(pld)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            break
+
 def ds_infer(args, audio_path):
 
+    from deepspeech.model import Model
     # Beam width used in the CTC decoder when building candidate transcriptions
     BEAM_WIDTH = 500
 
@@ -70,22 +127,30 @@ def ds_infer(args, audio_path):
 
     fs, audio = preprocess_audio(audio_path)
     audio_length = len(audio) * ( 1 / 16000)
-
-    print('Running inference.', file=sys.stderr)
     inference_start = timer()
     result = ds.stt(audio, fs)
     print(result)
     inference_end = timer() - inference_start
     print('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_length), file=sys.stderr)
+    return result
+     
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-if __name__ == '__main__':
-
-    audio = "./test/out.wav"
+def get_args():
     args = dict()
-    args['dir_path'] = "./deepspeech-models/"
+    dir_path = "./deepspeech-models/"
     args['model'] = dir_path+"output_graph.pb"
     args['lm'] = dir_path+"lm.binary"
     args['trie'] = dir_path+"trie"
     args['alphabet'] = dir_path+"alphabet.txt"
     ds_args = dotdict(args)
-    ds_infer(args, audio)
+    return ds_args
+ 
+if __name__ == '__main__':
+    #args = get_args()
+    #audio = "./test/out.wav"
+    #ds_infer(args, audio)
+    ds_server()
--- inference.py
+++ inference.py
@@ -1,11 +1,35 @@
-from utils import dotdict
-from libri_inference import libri_infer
-from ds_inference import ds_infer
+import python_bind
+import pickle
+import numpy as np
 
 audio = "./test/out.wav"
 
-def main():
+def preprocess_audio(audio_path):
+    import subprocess
+    import scipy.io.wavfile as wav
+    fs, audio = wav.read(audio_path)
+    if fs != 16000:
+        if fs < 16000:
+            print('Warning: original sample rate (%d) is lower than 16kHz. Up-sampling might produce erratic speech recognition.' % (fs), file=sys.stderr)
+        fs, audio = convert_samplerate(audio_path)
 
+    sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate 16000 - '.format(audio_path)
+    try:
+        p = subprocess.Popen(sox_cmd.split(),
+                             stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        output, err = p.communicate()
+
+        if p.returncode:
+            raise RuntimeError('SoX returned non-zero status: {}'.format(err))
+
+    except OSError as e:
+        raise OSError('SoX not found, use 16kHz files or install it: ', e)
+
+    audio = np.fromstring(output, dtype=np.int16)
+    return 16000, audio
+
+def get_args_edge():
+    from utils import dotdict, activation_functions_dict
     args = dict()
     # args['mode'] = 'test'
     # args['level'] = 'cha'
@@ -18,26 +42,47 @@ def main():
     args['num_feature'] = 39
     args['num_class'] = 29
     args['num_epochs'] = 1
-    args['savedir'] = './models/04232130'
-    libri_args = dotdict(args)
-
+    args['savedir'] = './models/04262030'
+    return dotdict(args)
+    
+def get_args_cloud():
+    from utils import dotdict
     args = dict()
-    args['dir_path'] = "./deepspeech-models/"
+    dir_path = "./deepspeech-models/"
     args['model'] = dir_path+"output_graph.pb"
     args['lm'] = dir_path+"lm.binary"
     args['trie'] = dir_path+"trie"
     args['alphabet'] = dir_path+"alphabet.txt"
-    ds_args = dotdict(args)
+    return dotdict(args)
 
+def get_results_edge(args, audio):
+    from libri_inference import libri_infer
 
+    libri_result, log_prob = libri_infer(args, audio)
+
+    return libri_result, log_prob
+
+def get_results_cloud(args, audio):
+    python_bind.init_edge()
+    (fs, audio) = preprocess_audio(audio)
+    data = pickle.dumps((fs,audio), 2)
+    python_bind.send(data)
+    ds_result = pickle.loads(python_bind.recv())
+    return ds_result
+
+def main():
     # Main Logic
-    libri_result, log_prob = libri_infer(libri_args, audio)
-    print("Edge Result:"+libri_result)
+    edge_args = get_args_edge()
+    cloud_args = get_args_cloud()
+    
+    edge_result, log_prob = get_results_edge(edge_args, audio)
+    print("Edge Result:\n"+edge_result)
+    print("Log Prob:"+str(log_prob))
     if (log_prob>0.1):
     	print("Result is good enough")
     else:
-    	ds_result = ds_infer(ds_args, audio)
-    	print("Cloud Result:"+ds_result)
+    	cloud_result = get_results_cloud(cloud_args, audio)
+    	print("Cloud Result:\n"+cloud_result)
 
 if __name__ == '__main__':
-	main()
+    main()
